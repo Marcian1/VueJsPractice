0776d093bca6ee34f96547f01625371889b48407
Marcian <marcian.bondoc@gmail.com>
2021-07-11 17:57:21 +0200

    Using nested routes

---
+++ src/components/nav/NotFound.vue
@@ -0,0 +1,3 @@
+<template>
+    <h2>Route not found. Please see our <router-link to="/teams">Teams</router-link></h2>
+</template>
--- src/main.js
+++ src/main.js
@@ -2,26 +2,36 @@ import { createApp } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router';
 import  TeamsList  from './components/teams/TeamsList.vue';
 import  UsersList  from './components/users/UsersList.vue';
-import  TeamMembers from '@/components/teams/TeamMembers'
+import  TeamMembers from '@/components/teams/TeamMembers';
+import NotFound from './components/nav/NotFound.vue';
 import App from './App.vue';
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         {
+            path:'/',
+            redirect:'/teams'
+        },
+        {
             path:'/users',
             component: UsersList
         },
         {
             path:'/teams',
-            component: TeamsList
+            component: TeamsList,
+            children:[
+                {
+                    path:':teamId',
+                    component: TeamMembers,
+                    props:true
+                },
+            ]
         },
         {
-            path:'/teams/:teamId',
-            component: TeamMembers,
-            props:true
-        },
-        
+            path:'/:notFound(.*)',
+            component: NotFound
+        }
     ]
 });
 const app = createApp(App)
