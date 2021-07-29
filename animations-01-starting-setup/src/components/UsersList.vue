e5d34e58b9451e5a3dcbd6f1c274c4db09d238e1
Marcian <marcian.bondoc@gmail.com>
2021-07-29 12:40:16 +0200

    Animating lists with transition-group

--- animations-01-starting-setup/src/components/UsersList.vue
+++ animations-01-starting-setup/src/components/UsersList.vue
@@ -1,8 +1,10 @@
 <template>
   <ul>
-    <li v-for="user in users" :key="user" @click="removeUser(user)">
-      {{ user }}
-    </li>
+    <transition-group name="users">
+      <li v-for="user in users" :key="user" @click="removeUser(user)">
+        {{ user }}
+      </li>
+    </transition-group>
   </ul>
   <input type="text" ref="inputText" />
   <button @click="addUser()">Add User</button>
@@ -38,7 +40,29 @@ li {
   border: 1px solid #ccc;
   padding: 1rem;
   text-align: center;
-  
+}
+
+.users-enter-from {
+  opacity: 0;
+  transform: translateX(-30px);
+}
+.users-enter-active {
+  transition: all 1s ease-out
+}
+
+.users-enter-to,
+.users-leave-from {
+  opacity: 1;
+  transform: translateX(0);
+}
+
+
+.users-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
+}
+.users-leave-active {
+  transition: all 1s ease-in
 }
 
 </style>
